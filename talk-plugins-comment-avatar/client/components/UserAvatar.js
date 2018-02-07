6261db6fc1f6e4d10a3727e5946f14f41a5b8a8c
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2018-02-07 15:15:35 +0800

    revise the default avatar

--- talk-plugins-comment-avatar/client/components/UserAvatar.js
+++ talk-plugins-comment-avatar/client/components/UserAvatar.js
@@ -12,15 +12,15 @@ var _styles = require('./styles.css');
 
 var _styles2 = _interopRequireDefault(_styles);
 
-var _avatarPlaceholder = require('../assets/avatar-placeholder.png');
+var _avatarReadr = require('../assets/avatar-readr.png');
 
-var _avatarPlaceholder2 = _interopRequireDefault(_avatarPlaceholder);
+var _avatarReadr2 = _interopRequireDefault(_avatarReadr);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var UserAvatar = function UserAvatar(_ref) {
   var user = _ref.comment.user;
-  return _react2.default.createElement('img', { src: !user.avatar ? _avatarPlaceholder2.default : user.avatar, className: _styles2.default.avatarPlaceholder });
+  return _react2.default.createElement('img', { src: !user.avatar ? _avatarReadr2.default : user.avatar, className: _styles2.default.avatarPlaceholder });
 };
 
 exports.default = UserAvatar;
--- talk-plugins-comment-avatar/lib/client/components/UserAvatar.js
+++ talk-plugins-comment-avatar/lib/client/components/UserAvatar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './styles.css';
-import avatarPlaceholder from '../assets/avatar-placeholder.png';
+import avatarPlaceholder from '../assets/avatar-readr.png';
 
 const UserAvatar = ({comment: {user}}) => 
   <img src={!user.avatar ? avatarPlaceholder : user.avatar} className={styles.avatarPlaceholder} />;
